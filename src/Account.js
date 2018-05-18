02295e9c993a1d4754e4fc051fafb58da3120495
Marcus Rettig <marcus0706@live.se>
2018-05-18 11:25:56 +0200

    Exercise Solution

--- src/Account.js
+++ src/Account.js
@@ -1,24 +1,27 @@
-/* eslint-disable class-methods-use-this, no-unused-vars */
-
 class Account {
   constructor(initialBalance) {
     this.balance = initialBalance;
+    this.transactions = [];
   }
 
   getBalance() {
-
+    return this.balance;
   }
 
   deposit(amount) {
-
+    this.balance += amount;
+    this.transactions.unshift(amount);
   }
 
   withdraw(amount) {
-
+    if (this.balance >= amount) {
+      this.balance -= amount;
+      this.transactions.unshift(-amount);
+    }
   }
 
   recentTransactions() {
-
+    return this.transactions.slice(0, 3);
   }
 }
 
--- src/Account.test.js
+++ src/Account.test.js
@@ -1,7 +1,6 @@
 const Account = require('./Account');
 
 describe('Account', () => {
-  /* eslint-disable-next-line no-unused-vars */
   let account;
 
   beforeEach(() => {
@@ -10,45 +9,56 @@ describe('Account', () => {
 
   describe('getBalance', () => {
     it('should get the current balance', () => {
-
+      expect(account.getBalance()).toEqual(1000);
     });
   });
 
   describe('deposit', () => {
     it('should increase the balance by the given amount', () => {
-
+      account.deposit(500);
+      expect(account.getBalance()).toEqual(1500);
     });
   });
 
   describe('withdraw', () => {
     it('should decrease the balance by the given amount', () => {
-
+      account.withdraw(1000);
+      expect(account.getBalance()).toEqual(0);
     });
 
     it('should not decrease the balance if account has insufficient funds', () => {
-
+      account.withdraw(1001);
+      expect(account.getBalance()).toEqual(1000);
     });
   });
 
   describe('recentTransactions', () => {
     it('should be empty if no transactions', () => {
-
+      expect(account.recentTransactions()).toEqual([]);
     });
 
     it('should contain deposits as positive numbers', () => {
-
+      account.deposit(300);
+      expect(account.recentTransactions()).toEqual([300]);
     });
 
     it('should contain withdrawals as negative numbers', () => {
-
+      account.withdraw(300);
+      expect(account.recentTransactions()).toEqual([-300]);
     });
 
     it('should have the last transaction first', () => {
-
+      account.deposit(200);
+      account.deposit(300);
+      expect(account.recentTransactions()).toEqual([300, 200]);
     });
 
     it('should contain the 3 most recent transactions', () => {
-
+      account.deposit(100);
+      account.deposit(200);
+      account.deposit(300);
+      account.deposit(100);
+      expect(account.recentTransactions()).toEqual([100, 300, 200]);
     });
   });
 });
